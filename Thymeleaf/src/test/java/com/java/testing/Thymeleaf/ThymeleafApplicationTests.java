6c84cdfc9bf37fc7452399122a2906f0631de345
TheintTheintThu <theinttheintthu.ucsy@gmail.com>
2019-10-22 14:39:37 +0630

    Testing SB Thymeleaf View

---
+++ Thymeleaf/src/test/java/com/java/testing/Thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.testing.Thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
